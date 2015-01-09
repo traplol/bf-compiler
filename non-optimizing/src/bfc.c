fe921c32f6f95fb73079b6a7ad04742ed27c0172
Max Mickey <max.mickey1991@gmail.com>
2015-01-09 04:31:23 -0800

    non-optimizing compiler now utilizes stdin/stdout

--- non-optimizing/src/bfc.c
+++ non-optimizing/src/bfc.c
@@ -16,14 +16,12 @@ FILE *OUT;
 FILE *IN;
 
 int indention = 0;
-
 void indent(void) {
     ++indention;
 }
 void dedent(void) {
     --indention;
 }
-
 char *tab(int num_tabs, char *str) {
     int len = 1 + num_tabs + strlen(str);
     char *s = calloc(1, len);
@@ -31,11 +29,9 @@ char *tab(int num_tabs, char *str) {
     memcpy(s+num_tabs, str, len-num_tabs);
     return s;
 }
-
 char *tabify(char *str) {
     return tab(indention, str);
 }
-
 void left(void) {
     fputs_tabbed("--" PTR_NAME ";\n", OUT);
 }
@@ -62,9 +58,7 @@ void close_bracket(void) {
     dedent();
     fputs_tabbed("}\n", OUT);
 }
-
-
-void init(void) {
+void header(void) {
     fputs_tabbed("#include <stdio.h>\n", OUT);
     fputs_tabbed("const int MEMSIZE = " MEMSIZE_sz ";\n", OUT);
     fputs_tabbed("int main() {\n", OUT);
@@ -72,22 +66,14 @@ void init(void) {
     fputs_tabbed("char " MEM_NAME "[MEMSIZE] = {0};\n", OUT);
     fputs_tabbed("char *" PTR_NAME " = mem;\n", OUT);
 }
-
-void denit(void) {
+void footer(void) {
     fputs_tabbed("return 0;\n", OUT);
     dedent();
-    fputs_tabbed("}\n", OUT);
+    fputs_tabbed("}\n\n", OUT);
 }
-
-void parse(char *filename) {
+void parse() {
     int c;
-
-    IN = fopen(filename, "r");
-    if (!IN) {
-        fprintf(stderr, "Could not open file \"%s\"\n", filename);
-        exit(1);
-    }
-    init();
+    header();
     while ((c = getc(IN)) != EOF) {
         switch (c) {
             case '>': right(); break;
@@ -103,17 +89,12 @@ void parse(char *filename) {
     }
     fclose(IN);
     IN = NULL;
-    denit();
+    footer();
 }
-
-int main(int argc, char **argv) {
-    --argc; ++argv;
-    if (!argc) {
-        fputs("No input file.\n", stderr);
-        return 1;
-    }
+int main(void) {
+    IN = stdin;
     OUT = stdout;
-    parse(*argv);
+    parse();
     return 0;
 }
 
