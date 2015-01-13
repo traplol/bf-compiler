35295b0cddc631aac15cf11008f0cf85f6830319
Max Mickey <max.mickey1991@gmail.com>
2015-01-13 01:58:02 -0800

    updating pass1 documentation TODO: pass1 no-ops

--- optimizing/inc/pass1.h
+++ optimizing/inc/pass1.h
@@ -1,11 +1,17 @@
 #ifndef _BF_COMPILER_OPTIMIZING_INC_PASS_1_H
 #define _BF_COMPILER_OPTIMIZING_INC_PASS_1_H
 
-/********************************************/
-/* Pass 1:                                  */
-/*  This pass handles constant folding for  */
-/*  the '<', '>', '+', and '-' instructions */
-/********************************************/
+/********************************************************************/
+/* Pass 1:                                                          */
+/*  This pass handles constant folding for the '<', '>', '+', and   */
+/*  '-'.                                                            */
+/*                                                                  */
+/*  '>>>' -> 'p += 3'                                               */
+/*  '++++--' -> '*p += 2'                                           */
+/*                                                                  */
+/*  TODO: Things like '>><<' and '++--' are removed because they    */
+/*          are no-ops.                                             */
+/********************************************************************/
 
 #include "emit.h"
 
